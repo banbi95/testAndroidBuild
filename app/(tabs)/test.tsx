e69a78842cfd7a285ddfaad8b13b0b47dbbf493b
Devin <15077487+org-devin@user.noreply.gitee.com>
2025-08-01 01:03:21 +0800

    increased heap size in setting

---
+++ app/(tabs)/test.tsx
@@ -0,0 +1,10 @@
+import {View,Text} from 'react-native'
+
+
+export default function myfucntion() {
+    return (
+        <View>
+            <Text> some text</Text>    
+        </View>
+    );
+}
